723848b7a9a2ecc2312708a62adbb1e9176f1d4f
Alexander Zheltov <voidmain02@gmail.com>
2019-07-25 02:00:56 +0500

    Проект Client вынуждено переписан на .net framework
    Под .net core не получилось настроить аттачмент дебага

---
+++ MiniAiCup.Paperio.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MiniAiCup.Paperio.Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MiniAiCup.Paperio.Client")]
+[assembly: AssemblyCopyright("Copyright © Alexander Zheltov 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("66295429-d284-4f93-a074-8798e1b60506")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
