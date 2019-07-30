c381ac48f8b17d9829b5ebe05d715e4cbd7faf80
Alexander Zheltov <voidmain02@gmail.com>
2019-07-31 01:23:18 +0500

    Скорректировано значение MaxTickCount в соответствии с раннером на сайте

--- MiniAiCup.Paperio.Core/Constants.cs
+++ MiniAiCup.Paperio.Core/Constants.cs
@@ -2,7 +2,7 @@ namespace MiniAiCup.Paperio.Core
 {
 	public static class Constants
 	{
-		public const int MaxTickCount = 1500;
+		public const int MaxTickCount = 1499;
 
 		public const string MyId = "i";
 	}
