44493ed80e032dea739f83d038b67ef610aacd6d
Alexander Zheltov <voidmain02@gmail.com>
2019-07-31 18:20:08 +0500

    Дополнены константы из исходников локал раннера

--- MiniAiCup.Paperio.Core/Constants.cs
+++ MiniAiCup.Paperio.Core/Constants.cs
@@ -2,6 +2,24 @@ namespace MiniAiCup.Paperio.Core
 {
 	public static class Constants
 	{
+		public const int BonusChance = 500;
+
+		public const int MaxBonusesCount = 3;
+
+		public const int NeutralTerritoryScore = 1;
+
+		public const int EnemyTerritoryScore = 5;
+
+		public const int SawScore = 30;
+
+		public const int LineKillScore = 50;
+
+		public const int SawKillScore = 150;
+
+		public const int MaxExecutionTime = 120;
+
+		public const int MaxRequestTime = 5;
+
 		public const int MaxTickCount = 1499;
 
 		public const string MyId = "i";
