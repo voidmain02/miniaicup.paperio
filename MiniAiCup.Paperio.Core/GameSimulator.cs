afbf4df60cb6b8c4a6ff4ae98568b723af49e1bc
Alexander Zheltov <voidmain02@gmail.com>
2019-08-03 00:09:07 +0500

    Причесан код симулятора

--- MiniAiCup.Paperio.Core/GameSimulator.cs
+++ MiniAiCup.Paperio.Core/GameSimulator.cs
@@ -9,6 +9,10 @@ namespace MiniAiCup.Paperio.Core
 
 		private List<PlayerInternal> _players;
 
+		private Dictionary<PlayerInternal, PointsSet> _capturedTerritoryPerPlayer;
+
+		private Dictionary<PlayerInternal, int> _scoresGainedPerPlayer;
+
 		private readonly IEnemyStrategy _enemyStrategy;
 
 		public GameSimulator(IEnemyStrategy enemyStrategy = null)
@@ -40,39 +44,37 @@ namespace MiniAiCup.Paperio.Core
 				}
 			}
 
-			var capturedPerPlayer = new Dictionary<PlayerInternal, PointsSet>();
-			var tickScoresPerPlayer = _players.ToDictionary(p => p, p => 0);
+			_capturedTerritoryPerPlayer = new Dictionary<PlayerInternal, PointsSet>();
+			_scoresGainedPerPlayer = _players.ToDictionary(p => p, p => 0);
 			foreach (var player in _players)
 			{
-				UpdatePlayerLines(player);
+				UpdatePlayerTail(player);
 
 				var capturer = new TerritoryCapturer(_mapSize, player.Territory);
 				var capturedTerritory = capturer.Capture(player.Tail);
-				capturedPerPlayer.Add(player, capturedTerritory);
+				_capturedTerritoryPerPlayer.Add(player, capturedTerritory);
 
 				if (capturedTerritory.Count > 0)
 				{
 					player.Tail = Path.Empty;
-					tickScoresPerPlayer[player] += Constants.NeutralTerritoryScore*capturedTerritory.Count;
+					_scoresGainedPerPlayer[player] += Constants.NeutralTerritoryScore*capturedTerritory.Count;
 				}
 			}
 
 			var losers = new List<PlayerInternal>();
 			foreach (var player in _players)
 			{
-				bool isLoss = CheckIsLoss(player, tickScoresPerPlayer);
-				if (isLoss)
+				if (CheckIsLoss(player))
 				{
 					losers.Add(player);
 				}
 			}
 
-			capturedPerPlayer = CollisionResolution(capturedPerPlayer);
+			ResolveCollisions();
 
 			foreach (var player in _players)
 			{
-				var isLossByPlayer = IsPlayerAte(player, capturedPerPlayer);
-				if (isLossByPlayer.Item1)
+				if (CheckIsPlayerAte(player))
 				{
 					losers.Add(player);
 				}
@@ -80,16 +82,18 @@ namespace MiniAiCup.Paperio.Core
 
 			foreach (var player in _players)
 			{
-				if (capturedPerPlayer[player].Count > 0)
+				if (_capturedTerritoryPerPlayer[player].Count <= 0)
 				{
-					player.Territory.UnionWith(capturedPerPlayer[player]);
-					foreach (var anotherPlayer in _players.Where(p => p != player))
-					{
-						int srcCount = anotherPlayer.Territory.Count;
-						anotherPlayer.Territory.ExceptWith(capturedPerPlayer[player]);
-						int croppedCount = anotherPlayer.Territory.Count;
-						tickScoresPerPlayer[player] += (Constants.EnemyTerritoryScore - Constants.NeutralTerritoryScore)*(srcCount - croppedCount);
-					}
+					continue;
+				}
+
+				player.Territory = player.Territory.UnionWith(_capturedTerritoryPerPlayer[player]);
+				foreach (var anotherPlayer in _players.Where(p => p != player))
+				{
+					int srcCount = anotherPlayer.Territory.Count;
+					anotherPlayer.Territory = anotherPlayer.Territory.ExceptWith(_capturedTerritoryPerPlayer[player]);
+					int croppedCount = anotherPlayer.Territory.Count;
+					_scoresGainedPerPlayer[player] += (Constants.EnemyTerritoryScore - Constants.NeutralTerritoryScore)*(srcCount - croppedCount);
 				}
 			}
 
@@ -100,29 +104,30 @@ namespace MiniAiCup.Paperio.Core
 
 			foreach (var player in _players)
 			{
-				player.Score += tickScoresPerPlayer[player];
+				player.Score += _scoresGainedPerPlayer[player];
 			}
 
 			return new GameStateInternal(tickNumber, _players, bonuses, state, move);
 		}
 
-		private Dictionary<PlayerInternal, PointsSet> CollisionResolution(Dictionary<PlayerInternal, PointsSet> capturedPerPlayer)
+		private void ResolveCollisions()
 		{
-			var p_to_c = capturedPerPlayer.Where(p => !IsPlayerAte(p.Key, capturedPerPlayer).Item1).ToDictionary(x => x.Key, x => x.Value);
-			var res = new Dictionary<PlayerInternal, PointsSet>(p_to_c);
+			var capturedTerritoryPerNotAtePlayer = _capturedTerritoryPerPlayer.Where(p => !CheckIsPlayerAte(p.Key))
+				.ToDictionary(x => x.Key, x => x.Value);
+			var result = new Dictionary<PlayerInternal, PointsSet>(capturedTerritoryPerNotAtePlayer);
 
-			foreach (var pair1 in p_to_c)
+			foreach (var pair1 in capturedTerritoryPerNotAtePlayer)
 			{
-				foreach (var pair2 in p_to_c)
+				foreach (var pair2 in capturedTerritoryPerNotAtePlayer)
 				{
 					if (pair1.Key != pair2.Key)
 					{
-						res[pair1.Key] = res[pair1.Key].ExceptWith(pair2.Value);
+						result[pair1.Key] = result[pair1.Key].ExceptWith(pair2.Value);
 					}
 				}
 			}
 
-			return res;
+			_capturedTerritoryPerPlayer = result;
 		}
 
 		private static void MovePlayer(PlayerInternal player, Move move)
@@ -136,7 +141,7 @@ namespace MiniAiCup.Paperio.Core
 			player.Position = nextPos;
 		}
 
-		private static void UpdatePlayerLines(PlayerInternal player)
+		private static void UpdatePlayerTail(PlayerInternal player)
 		{
 			if (!player.Territory.Contains(player.Position) || player.Tail.Length > 0)
 			{
@@ -144,28 +149,32 @@ namespace MiniAiCup.Paperio.Core
 			}
 		}
 
-		private bool CheckIsLoss(PlayerInternal player, IDictionary<PlayerInternal, int> tickScoresPerPlayer)
+		private bool CheckIsLoss(PlayerInternal player)
 		{
-			bool result = false;
+			// Переезд хвоста
+			bool isTailCutted = false;
+			var tailBeforeMove = player.Tail.RemoveFromEnd(1);
+			foreach (var anotherPlayer in _players)
+			{
+				if (tailBeforeMove.AsPointsSet().Contains(anotherPlayer.Position))
+				{
+					isTailCutted = true;
+					if (anotherPlayer != player)
+					{
+						_scoresGainedPerPlayer[anotherPlayer] += Constants.LineKillScore;
+					}
+				}
+			}
+
+			if (isTailCutted)
+			{
+				return true;
+			}
 
 			// Выход за границы карты
 			if (!_mapSize.ContainsPoint(player.Position))
 			{
-				result = true;
-			}
-
-			// Переезд хвоста
-			var linesBeforeMove = player.Tail.RemoveFromEnd(1);
-			foreach (var anotherPlayer in _players)
-			{
-				if (linesBeforeMove.Contains(anotherPlayer.Position))
-				{
-					result = true;
-					if (anotherPlayer != player)
-					{
-						tickScoresPerPlayer[anotherPlayer] += Constants.LineKillScore;
-					}
-				}
+				return true;
 			}
 
 			// Столкновение "лоб в лоб"
@@ -173,13 +182,9 @@ namespace MiniAiCup.Paperio.Core
 			{
 				foreach (var anotherPlayer in _players.Where(p => p != player))
 				{
-					if (player.Position == anotherPlayer.Position)
+					if (player.Position == anotherPlayer.Position && player.Tail.Length > anotherPlayer.Tail.Length)
 					{
-						if (player.Tail.Length > anotherPlayer.Tail.Length)
-						{
-							result = true;
-							break;
-						}
+						return true;
 					}
 				}
 			}
@@ -187,23 +192,15 @@ namespace MiniAiCup.Paperio.Core
 			// Остался без территории
 			if (player.Territory.Count == 0)
 			{
-				result = true;
+				return true;
 			}
 
-			return result;
+			return false;
 		}
 
-		private (bool, PlayerInternal) IsPlayerAte(PlayerInternal player, Dictionary<PlayerInternal, PointsSet> capturedPerPlayer)
+		private bool CheckIsPlayerAte(PlayerInternal player)
 		{
-			foreach (var capture in capturedPerPlayer.Where(x => x.Key != player))
-			{
-				if (capture.Value.Contains(player.Position))
-				{
-					return (true, capture.Key);
-				}
-			}
-
-			return (false, null);
+			return _capturedTerritoryPerPlayer.Where(x => x.Key != player).Any(capture => capture.Value.Contains(player.Position));
 		}
 
 		private static PlayerInternal ClonePlayer(PlayerInternal player)
