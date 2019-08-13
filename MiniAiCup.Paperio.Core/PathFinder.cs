284be4132479a655e605bca20cf4ab52f2bc742c
Alexander Zheltov <voidmain02@gmail.com>
2019-08-14 02:31:48 +0500

    Незначительные оптимизации в классе PathFinder

--- MiniAiCup.Paperio.Core/PathFinder.cs
+++ MiniAiCup.Paperio.Core/PathFinder.cs
@@ -5,6 +5,22 @@ namespace MiniAiCup.Paperio.Core
 {
 	public class PathFinder
 	{
+		private static readonly bool[,] Visited;
+
+		private static readonly bool[,] EmptyVisited;
+
+		private static readonly int[,] Moves;
+
+		private static readonly Queue<Point> Queue;
+
+		static PathFinder()
+		{
+			Queue = new Queue<Point>(Game.Params.MapLogicSize.Width*Game.Params.MapLogicSize.Height);
+			Visited = Game.GetNewMap<bool>();
+			EmptyVisited = Game.GetNewMap<bool>();
+			Moves = Game.GetNewMap<int>();
+		}
+
 		public static Path GetShortestPath(Point startPoint, PointsSet destinationPoints, PointsSet obstaclesPoints)
 		{
 			if (destinationPoints.Contains(startPoint))
@@ -12,57 +28,62 @@ namespace MiniAiCup.Paperio.Core
 				return Path.Empty;
 			}
 
-			var moves = Game.GetNewMap<int>();
-			var isVisited = Game.GetNewMap<bool>();
-			var queue = new Queue<Point>();
-			queue.Enqueue(startPoint);
-			isVisited[startPoint.X, startPoint.Y] = true;
-			moves[startPoint.X, startPoint.Y] = 0;
-			while (queue.Count > 0)
+			Reset();
+
+			Queue.Enqueue(startPoint);
+			Visited[startPoint.X, startPoint.Y] = true;
+			Moves[startPoint.X, startPoint.Y] = 0;
+			while (Queue.Count > 0)
 			{
-				var point = queue.Dequeue();
-				int currentPathLength = moves[point.X, point.Y];
+				var point = Queue.Dequeue();
+				int currentPathLength = Moves[point.X, point.Y];
 				foreach (var neighbor in point.GetNeighbors())
 				{
 					if (destinationPoints.Contains(neighbor))
 					{
-						moves[neighbor.X, neighbor.Y] = currentPathLength + 1;
-						return GetPath(moves, neighbor);
+						Moves[neighbor.X, neighbor.Y] = currentPathLength + 1;
+						return GetPath(neighbor);
 					}
 
-					if (Game.Params.MapLogicSize.ContainsPoint(neighbor) && !isVisited[neighbor.X, neighbor.Y] && !obstaclesPoints.Contains(neighbor))
+					if (Game.Params.MapLogicSize.ContainsPoint(neighbor) && !Visited[neighbor.X, neighbor.Y] && !obstaclesPoints.Contains(neighbor))
 					{
-						isVisited[neighbor.X, neighbor.Y] = true;
-						moves[neighbor.X, neighbor.Y] = currentPathLength + 1;
-						queue.Enqueue(neighbor);
+						Visited[neighbor.X, neighbor.Y] = true;
+						Moves[neighbor.X, neighbor.Y] = currentPathLength + 1;
+						Queue.Enqueue(neighbor);
 					}
 				}
-			}
-
-			Path GetPath(int[,] movesMap, Point currentPoint)
-			{
-				int currentLength = movesMap[currentPoint.X, currentPoint.Y];
-				var resultPath = new List<Point>(currentLength);
-
-				for (int i = currentLength - 1; i >= 0; i--)
-				{
-					resultPath.Add(currentPoint);
-					var validNeighbors = currentPoint.GetNeighbors().Where(p => Game.Params.MapLogicSize.ContainsPoint(p));
-					foreach (var validNeighbor in validNeighbors)
-					{
-						if (moves[validNeighbor.X, validNeighbor.Y] == i)
-						{
-							currentPoint = validNeighbor;
-							break;
-						}
-					}
-				}
-
-				resultPath.Reverse();
-				return new Path(resultPath);
 			}
 
 			return null;
+		}
+
+		private static Path GetPath(Point currentPoint)
+		{
+			int currentLength = Moves[currentPoint.X, currentPoint.Y];
+			var resultPath = new Point[currentLength];
+
+			for (int i = currentLength - 1; i >= 0; i--)
+			{
+				resultPath[i] = currentPoint;
+				var validNeighbors = currentPoint.GetNeighbors().Where(p => Game.Params.MapLogicSize.ContainsPoint(p));
+				foreach (var validNeighbor in validNeighbors)
+				{
+					if (Moves[validNeighbor.X, validNeighbor.Y] == i)
+					{
+						currentPoint = validNeighbor;
+						break;
+					}
+				}
+			}
+
+			return new Path(resultPath);
+		}
+
+		private static void Reset()
+		{
+			Queue.Clear();
+			Utils.FastCopyArray(EmptyVisited, Visited, Game.Params.MapLogicSize.Width*Game.Params.MapLogicSize.Height);
+			Utils.FastCopyArray(Game.NoEnemiesDangerousMap, Moves, Game.Params.MapLogicSize.Width*Game.Params.MapLogicSize.Height);
 		}
 	}
 }
