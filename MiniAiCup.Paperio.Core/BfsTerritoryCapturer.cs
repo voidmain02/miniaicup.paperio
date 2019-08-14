409de0619e55a5ebf1f3a1ddfa90985da08eb17d
Alexander Zheltov <voidmain02@gmail.com>
2019-08-14 14:05:54 +0500

    Оптимизирован захват территории

--- MiniAiCup.Paperio.Core/BfsTerritoryCapturer.cs
+++ MiniAiCup.Paperio.Core/BfsTerritoryCapturer.cs
@@ -1,90 +1,143 @@
 using System.Collections.Generic;
-using System.Linq;
 using MiniAiCup.Paperio.Core.Debug;
 
 namespace MiniAiCup.Paperio.Core
 {
 	public class BfsTerritoryCapturer
 	{
-		private readonly List<Point> _mapBoundaryPoints;
-
-		private readonly bool[,] _visited;
-
-		private readonly bool[,] _emptyVisited;
-
-		private readonly Queue<Point> _queue;
-
-		public BfsTerritoryCapturer()
-		{
-			_mapBoundaryPoints = GetBoundary(Game.Params.MapLogicSize).ToList();
-			_visited = Game.GetNewMap<bool>();
-			_emptyVisited = Game.GetNewMap<bool>();
-			_queue = new Queue<Point>(Game.Params.MapLogicSize.Width * Game.Params.MapLogicSize.Height);
-		}
-
-		public PointsSet Capture(PointsSet territory, IEnumerable<Point> tail)
+		public unsafe PointsSet Capture(PointsSet territory, IEnumerable<Point> tail)
 		{
 #if DEBUG
 			GameDebugData.Current.CaptureCount++;
 #endif
 
-			ResetVisited();
+			var visited = stackalloc byte[Game.Params.MapLogicSize.Width*Game.Params.MapLogicSize.Height];
+			const byte emptyCell = 0;
+			const byte territoryCell = 1;
+			const byte tailCell = 2;
+			const byte outsideCell = 3;
 
-			var usedPoints = territory.UnionWith(tail);
-			var startBoundaryPoints = _mapBoundaryPoints.Where(p => !usedPoints.Contains(p));
-			var outsidePoints = new List<Point>(startBoundaryPoints);
-			foreach (var outsidePoint in outsidePoints)
+			int minX = Game.Params.MapLogicSize.Width - 1;
+			int minY = Game.Params.MapLogicSize.Height - 1;
+			int maxX = 0;
+			int maxY = 0;
+
+			foreach (var point in tail)
 			{
-				_visited[outsidePoint.X, outsidePoint.Y] = true;
-				_queue.Enqueue(outsidePoint);
+				if (point.X < minX)
+				{
+					minX = point.X;
+				}
+				else if (point.X > maxX)
+				{
+					maxX = point.X;
+				}
+				if (point.Y < minY)
+				{
+					minY = point.Y;
+				}
+				else if (point.Y > maxY)
+				{
+					maxY = point.Y;
+				}
+				visited[point.X + point.Y*Game.Params.MapLogicSize.Width] = tailCell;
+			}
+			foreach (var point in territory)
+			{
+				if (point.X < minX)
+				{
+					minX = point.X;
+				}
+				else if (point.X > maxX)
+				{
+					maxX = point.X;
+				}
+				if (point.Y < minY)
+				{
+					minY = point.Y;
+				}
+				else if (point.Y > maxY)
+				{
+					maxY = point.Y;
+				}
+				visited[point.X + point.Y*Game.Params.MapLogicSize.Width] = territoryCell;
 			}
 
-			while (_queue.Count > 0)
+			int totalBoxSize = (maxX - minX + 1)*(maxY - minY + 1);
+			var queue = stackalloc int[totalBoxSize];
+			int queueHead = 0;
+			int queueTail = 0;
+
+			int outsideCount = 0;
+			for (int x = minX; x <= maxX; x++)
 			{
-				var point = _queue.Dequeue();
-				foreach (var neighbor in point.GetNeighbors())
+				if (visited[x + minY*Game.Params.MapLogicSize.Width] == emptyCell)
 				{
-					if (!Game.Params.MapLogicSize.ContainsPoint(neighbor))
-					{
-						continue;
-					}
-
-					if (_visited[neighbor.X, neighbor.Y])
-					{
-						continue;
-					}
-
-					_visited[neighbor.X, neighbor.Y] = true;
-					if (usedPoints.Contains(neighbor))
-					{
-						continue;
-					}
-					outsidePoints.Add(neighbor);
-					_queue.Enqueue(neighbor);
+					visited[x + minY*Game.Params.MapLogicSize.Width] = outsideCell;
+					outsideCount++;
+					queue[queueHead++] = x + minY*Game.Params.MapLogicSize.Width;
+				}
+				if (visited[x + maxY*Game.Params.MapLogicSize.Width] == emptyCell)
+				{
+					visited[x + maxY*Game.Params.MapLogicSize.Width] = outsideCell;
+					outsideCount++;
+					queue[queueHead++] = x + maxY*Game.Params.MapLogicSize.Width;
 				}
 			}
 
-			return Game.AllMapPoints.ExceptWith(outsidePoints).ExceptWith(territory);
-		}
-
-		private void ResetVisited()
-		{
-			Utils.FastCopyArray(_emptyVisited, _visited, Game.Params.MapLogicSize.Width*Game.Params.MapLogicSize.Height);
-		}
-
-		private static IEnumerable<Point> GetBoundary(Size size)
-		{
-			for (int i = 0; i < size.Width; i++)
+			for (int y = minY + 1; y <= maxY - 1; y++)
 			{
-				yield return new Point(i, 0);
-				yield return new Point(i, size.Height - 1);
+				if (visited[minX + y*Game.Params.MapLogicSize.Width] == emptyCell)
+				{
+					visited[minX + y*Game.Params.MapLogicSize.Width] = outsideCell;
+					outsideCount++;
+					queue[queueHead++] = minX + y*Game.Params.MapLogicSize.Width;
+				}
+				if (visited[maxX + y*Game.Params.MapLogicSize.Width] == emptyCell)
+				{
+					visited[maxX + y*Game.Params.MapLogicSize.Width] = outsideCell;
+					outsideCount++;
+					queue[queueHead++] = maxX + y*Game.Params.MapLogicSize.Width;
+				}
 			}
 
-			for (int i = 1; i < size.Height - 1; i++)
+			while (queueTail != queueHead)
 			{
-				yield return new Point(0, i);
-				yield return new Point(size.Width - 1, i);
+				int pointInt = queue[queueTail++];
+				var point = new Point(pointInt%Game.Params.MapLogicSize.Width, pointInt/Game.Params.MapLogicSize.Width);
+				foreach (var neighbor in point.GetNeighbors())
+				{
+					if (neighbor.X < minX || neighbor.X > maxX || neighbor.Y < minY || neighbor.Y > maxY)
+					{
+						continue;
+					}
+
+					if (visited[neighbor.X + neighbor.Y*Game.Params.MapLogicSize.Width] != emptyCell)
+					{
+						continue;
+					}
+
+					visited[neighbor.X + neighbor.Y*Game.Params.MapLogicSize.Width] = outsideCell;
+					outsideCount++;
+					queue[queueHead++] = neighbor.X + neighbor.Y*Game.Params.MapLogicSize.Width;
+				}
 			}
+
+			var result = new Point[totalBoxSize - outsideCount - territory.Count];
+			int index = 0;
+			for (int y = minY; y <= maxY; y++)
+			{
+				for (int x = minX; x <= maxX; x++)
+				{
+					if (visited[x + y*Game.Params.MapLogicSize.Width] == emptyCell ||
+						visited[x + y*Game.Params.MapLogicSize.Width] == tailCell)
+					{
+						result[index++] = new Point(x, y);
+					}
+				}
+			}
+
+			return new PointsSet(result);
 		}
 	}
 }
